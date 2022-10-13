198c9b7de7b5eb4cdf4c2100688043191b30aa55
franciscoha <54969936+franciscoha@users.noreply.github.com>
2022-10-13 15:36:10 -0500

    changes to use math.h

--- math.c
+++ math.c
@@ -1,10 +1,11 @@
 
 #include <stdio.h>
+#include "math.h"
     /*Funciones matematicas para nuestro codigo*/
 
 int doubleInt(int a){
     if( a>MAX_INT ){
-        return -1;""
+        return -1;
     }
     return a*2;
 }
--- readme.txt
+++ readme.txt
@@ -5,3 +5,4 @@ El directorio contendra algunos codigos en codigos en c los cuales
 se iran actualizando utilizando branches dentro de git.
 
 Nombre del alumno: FRANCISCO HERNANDEZ ALVAREZ
+Fecha de hoy: 13/10/2022
